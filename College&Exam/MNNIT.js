f175fc8589d63b3512228ba7686c482d622c1d83
Aryan Anand <aryanaa2005@gmail.com>
2026-01-01 20:13:19 +0530

    colleges data added

---
+++ College&Exam/MNNIT.js
@@ -0,0 +1,19 @@
+const mnnit = [
+  {
+    collegeId: "nit_allahabad",
+    name: "NIT Allahabad",
+    state: "Uttar Pradesh",
+    city: "Prayagraj",
+    stream: "Science_PCM",
+    courses: ["BTech"],
+    feesPerYear: 125000,
+    examRequired: ["jee_main"],
+    rating: 4.7,
+    location: {
+      lat: 25.492,
+      lng: 81.8639,
+    },
+  },
+];
+
+export default mnnit;
